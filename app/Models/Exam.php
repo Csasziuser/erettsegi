02f3c02a5eb612bf86b66860bb5b2f3564fddf1b
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2025-03-21 08:16:30 +0100

    Dénes - Model

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Exam extends Model
 {
     protected $fillable = ["classroom","subject","date"]; 
+    public function student()
+    {
+        return $this -> belongsTo(Student::class);
+    }
 }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Exam;
 
 class Student extends Model
 {
     protected $fillable = ["student_name","class"];
-    public function exam()
+    public function exams()
     {
-        
+        return $this -> hasMany(Exam::class);
     }
 }
